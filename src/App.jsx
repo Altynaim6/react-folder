0b628ee4f855012b7eb2699db0d945f9aa7d7ab4
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2025-01-10 18:37:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+
+const App = () => { //компонента это функция которая возв jsx
+    return (
+        <div className='App'>
+        </div>
+    );
+};
+
+export default App;
